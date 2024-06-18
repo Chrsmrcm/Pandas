552031cf2e30509adf8439d7b70161a03b8989bc
Justin Starr <jstarr11235@gmail.com>
2024-06-18 11:31:36 -0700

    data organization complete with 3 lists of labels,years in existence, and past week closing average

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,3 +1,7 @@
+'''
+CSV data will need to be sanitized.  Namely the founded year.  It is a str field and has no standardization
+'''
+
 import json
 import pandas
 import pathlib
@@ -9,20 +13,24 @@ config_path = pathlib.Path.cwd() / "shared" / "config.json"
 config_file = json.load(open(config_path))
 load_file = pathlib.Path(config_file["stock_analysis"]["load_file"])
 
-data = pandas.read_csv(load_file)
+csv_data = pandas.read_csv(load_file)
 #plot current stock price to years of existence (current year - founded)
 #test - get 7day average for closing cost
-symbols = list(data["Symbol"])
-closing_avg = [0 * len(symbols)]
-age = list(data["Founded"])
+symbols = list(csv_data["Symbol"])
 
-
+'''
+because the yahoo api needs a list of symbols as a parameter to pull initial data, we have to 
+reassign the symbols list to what is successfully pulled (some symbols in the spreadsheet might fail
+to return anything from yahoo
+'''
 #grab data from yahoo for each symbol in spreadsheet
 tickers = yfinance.download(" ".join(symbols), start=(today - datetime.timedelta(days=12)), end=today)
+symbols = list(tickers["Adj Close"].columns)
 
+closing_avg = []
+age = []
 for i in range(len(symbols)):
-    closing_avg[i] = tickers["Adj Close"][symbols[i]].mean()
-    age[i] = today.year - int(age[i])
+    closing_avg.append(tickers["Adj Close"][symbols[i]].mean())
+    age.append(today.year - int(csv_data[csv_data.Symbol == symbols[i]].Founded))
     
-print(closing_avg[:10])
-print(age[:10])
+
